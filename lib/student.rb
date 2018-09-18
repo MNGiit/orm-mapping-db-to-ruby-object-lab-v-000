ff1c020f02b4bd82dd5a88e49f7854a0f302d88d
Mohammed Noori <kmanoorib@gmail.com>
2018-09-18 05:37:56 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,13 +61,13 @@ class Student
     end
   end
 
-  def self.first_X_students_in_grade_10
+  def self.first_X_students_in_grade_10(number) #this function takes a number
     sql = <<-SQL
       SELECT *
       FROM students
       WHERE grade = 10
       ORDER BY students.id
-      LIMIT 1
+      LIMIT ?
     SQL
     
     #sql[0]
